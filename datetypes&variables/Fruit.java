b8e4fb5fc88bf1ac41ab7ecdd492377976b8700b
Megha1127 <meghanag1127@gmail.com>
2024-07-04 10:52:12 +0530

    codes on datatypes&variables

---
+++ datetypes&variables/Fruit.java
@@ -0,0 +1,18 @@
+class Fruit
+{
+	public static void main(String[] fruitBazar)
+	{
+		String fruitName ="licchi";
+		int price = 280;
+		String taste="sour";
+		String region=" karnataka ";
+		String colour="red";
+		
+		System.out.println("the fruit name is" + fruitName );
+		System.out.println(" the price of the fruit is" + price);
+		System.out.println(" the taste of the fruit is" + taste);
+		System.out.println(" the fruit region is" + region);
+		System.out.println(" the fruit colour is"+ colour);
+		
+	}
+}
---
+++ datetypes&variables/Shoes.java
@@ -0,0 +1,18 @@
+class Shoes
+{
+	public static void main(String[] shoesStore)
+	{
+		System.out.println(" the shoe colour is " + white);
+		System.out.println(" the size of the shoe is " + size);
+		System.out.println(" the shoe price is " + price);
+		System.out.println(" the shoe brand Id is " + brandId );
+		System.out.println(" the shoe material is " + material);
+		
+		String shoeColour = "white";
+		int size = 8;
+		int price = 2099;
+		int brandId = 1222;
+		String material = "converse";
+	}
+}	
+	
